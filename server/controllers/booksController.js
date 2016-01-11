3c000dc2bb64f4912999c670cfaa216f8db492a8
juvemar <martoo.atanasov@gmail.com>
2016-01-11 22:49:42 +0200

    Add book form a little changed and added bookDetails.jade to the views folder which will be used later when quitting the SPA.

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,33 +1,48 @@
 var Book = require('mongoose').model('Book');
 
 module.exports = {
-    getAllBooks: function(req, res, next) {
-        Book.find({}).exec(function(err, collection) {
+    getAllBooks: function (req, res, next) {
+        Book.find({}).exec(function (err, collection) {
             if (err) {
                 console.log('Books could not be loaded: ' + err);
             }
 
+            //res.render('booksList', {books: books});
             res.send(collection);
         })
     },
-    getBookById: function(req, res, next) {
-        Book.findOne({_id: req.params.id}).exec(function(err, book) {
+    getBookById: function (req, res, next) {
+        Book.findOne({_id: req.params.id}).exec(function (err, book) {
             if (err) {
                 console.log('Books could not be loaded: ' + err);
             }
 
+            //res.render('bookDetails', {book: book});
             res.send(book);
         })
     },
-    createBook: function(req, res, next) {
+    createBook: function (req, res, next) {
         var newBookData = req.body;
-        Book.create(newBookData, function(err, book){
+        Book.create(newBookData, function (err, book) {
             if (err) {
                 console.log('Failed to create new book: ' + err);
                 return;
             }
 
+            //res.render('addBooks', {book: books});
             res.send(book);
         })
+    },
+    updateBook: function (req, res, next) {
+        if (req.user.roles.indexOf('admin') > -1) {
+            var updatedBookData = req.body;
+
+            Book.update({_id: req.body._id}, updatedBookData, function () {
+                res.end();
+            })
+        }
+        else {
+            res.send({reason: 'You do not have permissions!'})
+        }
     }
 };
