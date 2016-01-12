e1d366607626bf8c77ae49d543e6f2a7b44ca049
juvemar <martoo.atanasov@gmail.com>
2016-01-13 00:04:58 +0200

    Starting to change from SPA to MVC. Pray for us!

--- public/app/app.js
+++ public/app/app.js
@@ -1,99 +1,38 @@
-var app = angular.module('app', ['ngResource', 'ngRoute']).value('toastr', toastr);
+(function () {
 
-app.config(function($routeProvider, $locationProvider) {
-    // $locationProvider.html5Mode(true);
+    'use strict';
 
-    var routeUserChecks = {
-        adminRole: {
-            authenticate: function(auth) {
-                return auth.isAuthorizedForRole('admin');
+    function config() {
+
+        var routeUserChecks = {
+            adminRole: {
+                authenticate: function(auth) {
+                    return auth.isAuthorizedForRole('admin');
+                }
+            },
+            authenticated: {
+                authenticate: function(auth) {
+                    return auth.isAuthenticated();
+                }
             }
-        },
-        isBanned: {
-            authenticate: function(auth) {
-                return auth.isAuthorizedForRole('banned');
-            }
-        },
-        authenticated: {
-            authenticate: function(auth) {
-                return auth.isAuthenticated();
-            }
-        }
-    };
+        };
+    }
 
-    $routeProvider
-        .when('/', {
-            templateUrl: '/partials/main/home',
-            controller: 'MainCtrl'
+    function run($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function(ev, current, previous, rejection) {
+            if (rejection === 'not authorized') {
+                $location.path('/');
+            }
         })
-        .when('/books', {
-            templateUrl: '/partials/books/books-list',
-            controller: 'BooksListCtrl'
-        })
-        .when('/books/edit/', {
-            templateUrl: '/partials/books/book-publish',
-            controller: 'BookPublishCtrl'
-        })
-        .when('/books/edit/:id', {
-            templateUrl: '/partials/books/book-edit',
-            controller: 'BookEditCtrl'
-        })
-        .when('/books/:id', {
-            templateUrl: '/partials/books/book-details',
-            controller: 'BookDetailsCtrl'
-        })
-        .when('/books/:id/review', {
-            templateUrl: '/partials/books/book-review',
-            controller: 'BookReviewCtrl'
-        })
-        .when('/movies', {
-            templateUrl: '/partials/movies/movies-list',
-            controller: 'MoviesListCtrl'
-        })
-        .when('/movies/edit/', {
-            templateUrl: '/partials/movies/movie-publish',
-            controller: 'MoviePublishCtrl'
-        })
-        .when('/movies/edit/:id', {
-            templateUrl: '/partials/movies/movie-edit',
-            controller: 'MovieEditCtrl'
-        })
-        .when('/movies/:id', {
-            templateUrl: '/partials/movies/movie-details',
-            controller: 'MovieDetailsCtrl'
-        })
-        .when('/movies/:id/review', {
-            templateUrl: '/partials/movies/movie-review',
-            controller: 'MovieReviewCtrl'
-        })
-        .when('/games', {
-            templateUrl: '/partials/games/games-list',
-            controller: 'GamesListCtrl'
-        })
-        .when('/games/:id', {
-            templateUrl: '/partials/games/game-details',
-            controller: 'GameDetailsCtrl'
-        })
-        .when('/signup', {
-            templateUrl: '/partials/account/signup',
-            controller: 'SignUpCtrl'
-        })
-        .when('/profile', {
-            templateUrl: '/partials/account/profile',
-            controller: 'ProfileCtrl',
-            resolve: routeUserChecks.authenticated
-        })
-        .when('/admin/users', {
-            templateUrl: '/partials/admin/users-list',
-            controller: 'UserListCtrl',
-            resolve: routeUserChecks.adminRole
-        })
-});
+    }
 
-app.run(function($rootScope, $location) {
-    $rootScope.$on('$routeChangeError', function(ev, current, previous, rejection) {
-        if (rejection === 'not authorized') {
-            $location.path('/');
-        }
-    })
-});
+    angular.module('app.services', []);
+    angular.module('app.directives', []);
+    angular.module('app.filters', []);
+    angular.module('app.controllers', ['app.services']);
+    angular.module('app', ['ngResource', 'ngRoute', 'app.services', 'app.directives', 'app.filters', 'app.controllers'])
+        .config([config])
+        .run(['$rootScope', '$location', run])
+        .value('toastr', toastr)
+        .constant('baseServiceUrl', 'http://localhost:3000/');
+}());
