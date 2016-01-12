555e6be6d6e5e65f3c792dccb065156f06dfe191
Gergana Belcheva <gergana.belcheva@gmail.com>
2016-01-12 02:39:34 +0200

    OOP-ed models

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,35 +1,40 @@
 var mongoose = require('mongoose');
+var item = require('./Item');
 
-var booksSchema = mongoose.Schema({
-    type: String,
-    title: String,
-    author: String,
-    ISBN: Number,
-    published: Number,
-    tags: [String],
-    genres: [String],
-    price: Number,
-    quantity: Number,
-    description: String,
-    reviews: [{
-        author: String,
-        content: String,
-        rating: Number
-    }],
-    imageUrl: String
-});
+var book = function () {
+    var book;
 
-var Book = mongoose.model('Book', booksSchema);
+    book = Object.create(item);
 
-module.exports.seedInitialBooks = function() {
-    Book.find({}).exec(function(err, collection) {
+    Object.defineProperty(book, 'init', {
+        value: function () {
+            item().init.call(this);
+
+            this.genres = [String];
+            this.author = String;
+            this.ISBN = Number;
+            this.published = Number;
+
+            return this;
+        }
+    });
+
+    return book;
+};
+
+var bookSchema = mongoose.Schema(book().init());
+var Book = mongoose.model('Book', bookSchema);
+
+module.exports.seedInitialBooks = function () {
+    Book.find({}).exec(function (err, collection) {
         if (err) {
             console.log('Cannot find book: ' + err);
             return;
         }
 
         if (collection.length === 0) {
-            Book.create({type: 'book',
+            Book.create({
+                type: 'book',
                 title: 'LOTR',
                 author: 'AZ',
                 ISBN: 9780261103566,
@@ -44,9 +49,10 @@ module.exports.seedInitialBooks = function() {
                     content: 'Alabala',
                     rating: 5
                 }],
-                imageUrl: 'https://www.orangecenter.bg/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/9/7/9780261103566.jpg'});
+                imageUrl: 'https://www.orangecenter.bg/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/9/7/9780261103566.jpg'
+            });
             console.log('book added');
-            }
+        }
 
 
     });
